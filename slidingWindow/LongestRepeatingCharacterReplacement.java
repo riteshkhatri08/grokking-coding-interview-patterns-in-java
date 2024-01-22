494019c90b59ad6514d9e15a20aee18a1e0a7422
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-01-23 03:38:22 +0530

    Corrected input for educative variant  of LongestRepeatingCharacterReplacement

--- slidingWindow/LongestRepeatingCharacterReplacement.java
+++ slidingWindow/LongestRepeatingCharacterReplacement.java
@@ -1,15 +1,18 @@
+// https://www.educative.io/courses/grokking-coding-interview-patterns-java/longest-repeating-character-replacement
+// https://leetcode.com/problems/longest-repeating-character-replacement/description/
+
 package slidingWindow;
 
 public class LongestRepeatingCharacterReplacement {
     public static void main(String[] args) {
-        String s = "ABBB";
+        String s = "aaacbbbaabab";
         int k = 2;
         System.out.println("ANSWER = " + longestRepeatingCharacterReplacement(s, k));
     }
 
     public static int longestRepeatingCharacterReplacement(String s, int k) {
-        
-        int[] charcount = new int[91];
+
+        int[] charcount = new int[123];
 
         int left = 0, right = 0, maxsofar = 0;
 
@@ -21,13 +24,13 @@ public class LongestRepeatingCharacterReplacement {
         maxchar = charray[left];
         for (; right < charray.length; right++) {
 
-            // System.out.println("NEW CHAR CAME IN - " + charray[right] + " , right=" +
-            // right);
+            System.out.println("NEW CHAR CAME IN - " + charray[right] + " , right=" +
+                    right);
 
             // Increment count of current character
             charcount[charray[right]]++;
 
-            // System.out.println("CHAR COUNT = " + charcount);
+            System.out.println("CHAR COUNT = " + print(charcount));
 
             // Check if current character same as max character
             if (maxchar != charray[right]) {
@@ -35,18 +38,18 @@ public class LongestRepeatingCharacterReplacement {
                 // Check if there's change in max char due to new character
                 if (charcount[maxchar] < charcount[charray[right]]) {
                     maxchar = charray[right];
-                    // System.out.println(" New max xhar " + maxchar);
+                    System.out.println(" New max xhar " + maxchar);
                 } else {
                     // Check how many characters have we replaced already
-                    // System.out.println("Replaced = " + replaced);
+                    System.out.println("Replaced = " + (right - left - charcount[maxchar] + 1));
                     while ((right - left - charcount[maxchar] + 1) > k) {
 
                         // time to shrink window
                         charcount[charray[left]]--;
                         left++;
 
-                        // System.out.println("decrease window, char count" + charcount + " , left=" +
-                        // left);
+                        System.out.println("decrease window, char count" + print(charcount) + " , left=" +
+                                left);
 
                         // Now get the new max char
                         maxchar = getMostRecurringCharacter(charcount);
@@ -56,8 +59,8 @@ public class LongestRepeatingCharacterReplacement {
 
             } else {
                 // Then no issue, we as we are not increasing replace count
-                if ((right - left) > maxsofar) {
-                    maxsofar = right - left;
+                if ((right - left + 1) > maxsofar) {
+                    maxsofar = right - left + 1;
                 }
 
             }
@@ -72,12 +75,24 @@ public class LongestRepeatingCharacterReplacement {
     }
 
     public static char getMostRecurringCharacter(int[] charcount) {
-        int maxindex = 65;
-        for (int curindex = 65; curindex < charcount.length; curindex++) {
+        int maxindex = 97;
+        for (int curindex = 97; curindex < charcount.length; curindex++) {
             if (charcount[maxindex] < charcount[curindex]) {
                 maxindex = curindex;
             }
         }
         return (char) (maxindex);
+    }
+
+    public static String print(int[] charcount) {
+        // StringBuidler sb = new StringBuidler();
+        String s = "[";
+        for (int c = 97; c < charcount.length; c++) {
+            if (charcount[c] != 0)
+                s += (char) c + "=" + charcount[c] + ", ";
+
+        }
+        s += "]";
+        return s;
     }
 }

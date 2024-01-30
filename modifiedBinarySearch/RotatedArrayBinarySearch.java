a2b798ffc1a7e04bc5678acb6d00c226e2e93515
Ritesh Khatri <riteshkhatri1512@gmail.com>
2024-01-30 23:22:51 +0530

    Optimized Solution for RotatedArrayBinarySearch

--- modifiedBinarySearch/RotatedArrayBinarySearch.java
+++ modifiedBinarySearch/RotatedArrayBinarySearch.java
@@ -1,88 +1,102 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+// https://www.codingninjas.com/studio/problems/search-in-rotated-sorted-array_1082554?leftPanelTabValue=SUBMISSION
+// https://www.educative.io/courses/grokking-coding-interview-patterns-java/search-in-rotated-sorted-array
+
 package modifiedBinarySearch;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class RotatedArrayBinarySearch {
-    public static void main(String[] args) {
-        List<Integer> input = Arrays.asList(1,2);
-        int target = 2;
-        int result = binarySearchRotated(input, target);
-        System.out.println("ANSWER = " + result);
+    public static void main(String[] args) throws Exception {
+        // List<Integer> input = Arrays.asList(-9996, -9941, 1);
+        // int target = 20;
+        int[] input = new int[] { 3, 1 };
+        for (int i = 0; i < input.length; i++) {
+            System.out.println("=====================\nSearching for " + input[i]);
+            int result = new RotatedArrayBinarySearch().search(input, input[i]);
+            if (i != result) {
+                throw new Exception("RESULT and EXPECTED MISMATCH");
+            }
+            System.out.println("ANSWER = " + result);
+        }
     }
 
-    public static int binarySearchRotated(List<Integer> nums, int target) {
-        if (nums.size() == 1) {
-            if (nums.get(0) == target) {
+    //
+
+    // Use binary search to find pivot
+    // if target < = left and <= pivot find in left arrat else in right array
+    public int search(int[] nums, int target) {
+
+        if (nums.length == 1) {
+            if (nums[0] == target) {
                 return 0;
             } else {
                 return -1;
             }
         }
-        int left1 = 1, right2 = nums.size() - 2, right1 = -1, left2 = -1;
-        // Check only if left1 is greater than right2
-        // this means array has been rotated atleast once;
-        if (nums.get(left1) > nums.get(right2)) {
 
-            // Find Rotation Point by using pointers from both sides, for fast search
-            while (right2 >= left1) {
-                if (nums.get(left1) < nums.get(left1 - 1)) {
-                    right1 = left1 - 1;
-                    left2 = left1;
-                    break;
-                    // This is rotation point
-                } else if (nums.get(right2) > nums.get(right2 + 1)) {
-                    right1 = right2;
-                    left2 = right2 + 1;
-                    break;
-                }
-                right2--;
-                left1++;
-            }
-            if (right2 < left1) {
-                if (nums.get(left1) < nums.get(right2)) {
-                    right1 = right2;
-                    left2 = left1;
-                }
-            }
-            right2 = nums.size() - 1;
-            left1 = 0;
-            // Now if target is greater than element at left 1 that means it is in first sub
-            // array else it is in second hafl
-            // do a binary search on which ever half it is present in ;
-            if (target >= nums.get(left1)) {
-                if (target == nums.get(left1)) {
-                    return left1;
-                } else {
-                    // do binary search from left1 to right1
-                    return binarySearch(left1, right1, nums, target);
-                }
+        int pivot = findPivot(nums);
+        if (pivot >= 0) {
+
+            if (target >= nums[0]) {
+                return binarySearch(0, pivot, nums, target);
             } else {
-                // do a binarysearch from left2 to right2
-                return binarySearch(left2, right2, nums, target);
+                return binarySearch(pivot + 1, nums.length - 1, nums, target);
             }
 
         } else {
-            // DO normal binary search on complete array
-            return binarySearch(0, nums.size() - 1, nums, target);
+            return binarySearch(0, nums.length - 1, nums, target);
         }
-
     }
 
-    private static int binarySearch(int left, int right, List<Integer> nums, int target) {
+    private static int findPivot(int[] nums) {
+
+        int left = 0, right = nums.length - 1, mid, n = right;
+
+        while (left <= right) {
+            mid = (left + right) / 2;
+            // EDGE CASE 1
+            if (mid == 0) {
+                if (nums[mid] <= nums[mid + 1]) {
+                    return -1;
+                } else {
+                    return mid;
+                }
+            }
+            // EDGE CASE 2
+            if (mid == n) {
+                if (nums[mid] <= nums[mid - 1]) {
+                    return mid - 1;
+                } else {
+                    return -1;
+                }
+            }
+
+            // Left of mid greater than mid
+            if (nums[mid - 1] > nums[mid]) {
+                // THEN mid -1 is pivot
+                return mid - 1;
+            } else if (nums[mid] > nums[0]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+
+        return -1;
+    }
+
+    private static int binarySearch(int left, int right, int[] nums, int target) {
         int mid;
         while (left <= right) {
             mid = (left + right) / 2;
-            System.out.println("mid = " + nums.get(mid) + " target = " + target);
-            if (target == nums.get(mid)) {
+            if (target == nums[mid]) {
                 return mid;
-            } else if (target < nums.get(mid)) {
+            } else if (target < nums[mid]) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-
         return -1;
     }
 }
